1e2d162a890fad792ff7472a670001a6cf68d4fe
juarezjunior256 <juarezjunior.live@gmail.com>
2022-02-15 00:16:28 -0300

    Criptografando senha do usuário

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -2,6 +2,7 @@ import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import User from '@modules/users/typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
+import { hash } from 'bcryptjs';
 
 interface IRequest {
   name: string;
@@ -18,10 +19,13 @@ class CreateUserService {
       throw new AppError('E-mail já cadastrado');
     }
 
+    // criptografando senha do usuário
+    const hashedPassword = await hash(password, 8);
+
     const user = userRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await userRepository.save(user);
