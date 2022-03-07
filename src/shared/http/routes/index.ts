7b28c054093fd387b1fd0d74287a2b588dc0488a
juarezjunior256 <juarezjunior.live@gmail.com>
2022-03-07 01:06:36 -0300

    Finalizada rota de vendas

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -4,6 +4,7 @@ import sessionRouter from '@modules/users/routes/sessions.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
 import profileRouter from '@modules/users/routes/profile.routes';
 import customersRouter from '@modules/customers/routes/customers.routes';
+import ordersRouter from '@modules/orders/routes/orders.routes';
 import { Router } from 'express';
 
 const routes = Router();
@@ -14,5 +15,6 @@ routes.use('/sessions', sessionRouter);
 routes.use('/password', passwordRouter);
 routes.use('/profile', profileRouter);
 routes.use('/customers', customersRouter);
+routes.use('/orders', ordersRouter);
 
 export default routes;
