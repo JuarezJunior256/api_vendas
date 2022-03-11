5bc4c9eabce5154da97fa84ebada4bc52683c3a5
juarezjunior256 <juarezjunior.live@gmail.com>
2022-03-11 00:40:54 -0300

    cache configurado

--- src/modules/products/services/DeleteProductService.ts
+++ src/modules/products/services/DeleteProductService.ts
@@ -1,3 +1,4 @@
+import RedisCache from '@shared/cache/RedisCache';
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
@@ -16,6 +17,9 @@ class DeleteProductService {
     if (!product) {
       throw new AppError('Produto não encontrado');
     }
+
+    const redisCache = new RedisCache();
+    await redisCache.invalidate('api-vendas-PRODUCT_LIST');
 
     await productsRepository.delete(id);
   }
