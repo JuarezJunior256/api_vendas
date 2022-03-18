9f7e0a28f14d51b05adab15f5bb36d98a3c376d4
juarezjunior256 <juarezjunior.live@gmail.com>
2022-03-17 22:52:47 -0300

    finalizando middleware do rateLimiter

--- src/shared/http/middlewares/rateLimiter.ts
+++ src/shared/http/middlewares/rateLimiter.ts
@@ -1,10 +1,30 @@
 import { Request, Response, NextFunction } from 'express';
-import redis from 'redis';
+import Redis from 'ioredis';
 import { RateLimiterRedis } from 'rate-limiter-flexible';
 import AppError from '../../errors/AppError';
 
-const redisClient = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT),
-  password: process.env.REDIS_PASS || undefined,
-});
+export default async function ratelimit(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): Promise<void> {
+  try {
+    const redisClient = new Redis({
+      host: process.env.REDIS_HOST,
+      port: Number(process.env.REDIS_PORT),
+      password: process.env.REDIS_PASS || undefined,
+    });
+
+    const limiter = new RateLimiterRedis({
+      storeClient: redisClient,
+      keyPrefix: 'ratelimit',
+      points: 2,
+      duration: 1,
+    });
+    await limiter.consume(request.ip);
+
+    return next();
+  } catch (err) {
+    throw new AppError('Muitas requisições nessa rota', 429);
+  }
+}
